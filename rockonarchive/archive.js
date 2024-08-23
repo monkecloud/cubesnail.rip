901a03c3c8b58f4e3dec578226418d53b841c3b1
Cubesnail <cubesnail@gmail.com>
2024-08-23 00:19:11 -0400

    slab reset

--- rockonarchive/archive.js
+++ rockonarchive/archive.js
@@ -1,9 +1,17 @@
-
 const req = new XMLHttpRequest();
-req.open("GET",'sets.json',true);
-req.send();
-req.onload = function(){
-  const json = JSON.parse(req.responseText);
-  document.getElementById('testdiv').innerHTML = JSON.stringify(json);
-};
+function loadXMLDoc(){
+  req.open("GET",'sets.json',true);
+  req.send();
+  req.onload = function(){
+    const json = JSON.parse(req.responseText);
+    //document.getElementById('testdiv').innerHTML = JSON.stringify(json);
+    
+    htmlOutput = "";
+    htmlDiv = document.getElementById("sets");
+    json.archive.forEach(function(val){
+      htmlOutput += '<div class="set"'
+     });
+    console.log(htmlOutput)
+  };
+}
 document.getElementById("buttontest").onclick = loadXMLDoc;
