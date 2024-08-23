071807eca024769ea49f30e5e4e9a0f65b5a4f2b
Cubesnail <cubesnail@gmail.com>
2024-08-23 18:57:24 -0400

    August 23rd slab backfill

--- rockonarchive/archive.js
+++ rockonarchive/archive.js
@@ -8,10 +8,16 @@ function loadXMLDoc(){
     
     htmlOutput = "";
     htmlDiv = document.getElementById("sets");
-    json.archive.forEach(function(val){
-      htmlOutput += '<div class="set"'
+    json.archive.forEach(function(val, i){
+      const sets = Object.keys(val)
+      sets.forEach(
+        htmlOutput+='<div class="set" id="set' + i + '"> </div>'
+      )
+
+      console.log(sets)
      });
     console.log(htmlOutput)
   };
 }
 document.getElementById("buttontest").onclick = loadXMLDoc;
+ 
